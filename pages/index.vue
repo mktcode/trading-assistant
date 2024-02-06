040598c07bcda908949013cb98e5f202f30ca86b
mktcode <kontakt@markus-kottlaender.de>
2024-02-06 11:04:18 +0100

    style

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,9 @@ onMounted(() => {
   <div class="flex flex-col container mx-auto py-12">
     <ClientOnly>
       <SelectPair class="mb-12 mx-auto" />
-      <Trades v-for="pair in watchedPairs" :pair="pair" :key="pair" />
+      <div class="flex space-x-6">
+        <Trades v-for="pair in watchedPairs" :pair="pair" :key="pair" />
+      </div>
     </ClientOnly>
   </div>
 </template>
